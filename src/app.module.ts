3a99eb2fce829e0cced9033a110c5a03e0bd10f6
Fikru Kebede <fikrukebede90@gmail.com>
2025-08-08 17:43:22 +0300

    update import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from '@mguay/nestjs-better-auth';
-import { auth } from 'utils/auth';
+import { auth } from '../utils/auth';
 import { PrismaService } from './prisma/prisma.service';
 @Module({
   imports: [AuthModule.forRoot(auth), AuthModule],
