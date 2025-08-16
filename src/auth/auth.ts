7552931f477388a7eb32988152d206a51d952185
Fikru Kebede <fikrukebede90@gmail.com>
2025-08-16 10:25:48 +0300

    minor fix

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,11 +1,13 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { organization } from 'better-auth/plugins';
 
-export const auth:any = betterAuth({
+export const auth: any = betterAuth({
   database: drizzleAdapter(
     {},
     {
       provider: 'pg',
     },
   ),
+  plugins: [organization()],
 });
